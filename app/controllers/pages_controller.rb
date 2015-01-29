cd60e29256e69805c32b6ed84639e924424e27cd
Jeff Delaney <hello@codediode.io>
2015-01-28 22:25:08 -0800

    updated soft user feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
 
   def home
+    @token = current_user.soft_user_token
     @user = current_user
     @recipes = Recipe.paginate(:page => params[:page], :per_page => 12)
     @user_recipes = @user.recipes.limit(3)
+    @soft_recipes = @recipes.where(soft_user_token: @token)
   end
 
   def about
